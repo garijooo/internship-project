2bec0a5a9d32eac55ea0e2019e4746e4971b51bf
Egor Matveyas <yogafeed@gmail.com>
2021-06-23 14:05:38 +0300

    rename of the root homepage

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Login from './views/Login/Login';
 import SignUp from './views/SignUp/SignUp';
 import PasswordUpdate from './views/PasswordUpdate/PasswordUpdate';
 import PasswordReset from './views/PasswordReset/PasswordReset';
-import Profile from './views/Profile/Profile';
+import Streams from './views/Streams/Streams';
 import './styles/index.css';
 import './styles/auth.css';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Profile} />
+      <Route exact path="/" component={Streams} />
       <Route exact path="/auth/login" component={Login} />
       <Route exact path="/auth/signup" component={SignUp} />
       <Route exact path="/auth/password/update/:resetToken" component={PasswordUpdate} />
