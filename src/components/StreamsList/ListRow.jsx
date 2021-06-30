2f816d3323545696859a27f1e9a35547fc2a1856
Egor Matveyas <yogafeed@gmail.com>
2021-07-01 01:12:35 +0300

    submenus dynamic display added

--- src/components/StreamsList/ListRow.jsx
+++ src/components/StreamsList/ListRow.jsx
@@ -1,27 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { AiOutlineMore } from 'react-icons/ai';
+import classNames from 'classnames';
+import OptionalMenu from '../OptionalMenu/OptionalMenu';
 import styles from './StreamsList.module.css';
 
-const ListRow = () => (
-  <>
-    <div className={styles.row}>
-      <div>Frontend Dev. stream 03</div>
-      <div>22 Jun 2021</div>
-      <div>31 days</div>
-      <div>20</div>
-      <div>Igor Bariev</div>
-      <div>Mark Ovchinnikov</div>
-      <div>Oncomming</div>
-    </div>
-    <div className={styles.row}>
-      <div>C++ Dev. stream 08</div>
-      <div>01 Jun 2021</div>
-      <div>15 days</div>
-      <div>18</div>
-      <div>Anastasia Marchenko</div>
-      <div>Maxim Osipenko</div>
-      <div>Active</div>
-    </div>
-  </>
-);
+const ListRow = ({
+  stream,
+  index,
+  onClickHandler,
+  currentExpand,
+}) => {
+  // const [isExpanded, setIsExpanded] = useState(false);
+  const statusClass = classNames({
+    [styles.oncoming]: stream.status === 'Oncoming',
+    [styles.active]: stream.status === 'Active',
+    [styles.paused]: stream.status === 'Paused',
+  });
+
+  const submenuClass = classNames(styles.submenu, {
+    [styles.hidden]: currentExpand !== index,
+  });
+  const iconClass = classNames({
+    [styles.expanded]: currentExpand === index,
+  });
+
+  const onExpand = () => {
+    if (currentExpand === index) onClickHandler(null);
+    else onClickHandler(index);
+  };
+
+  return (
+    <tr className={styles.row}>
+      <td className={styles.name}>{stream.name}</td>
+      <td>{stream.date}</td>
+      <td>{stream.duration}</td>
+      <td>{stream.interns}</td>
+      <td>{stream.mentor}</td>
+      <td>{stream.lead}</td>
+      <td><span className={statusClass}>{stream.status}</span></td>
+      <td>
+        <div className={styles.more}>
+          <div className={submenuClass}>
+            <OptionalMenu />
+          </div>
+          <AiOutlineMore onClick={onExpand} className={iconClass} />
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+ListRow.propTypes = {
+  stream: PropTypes.objectOf({
+    name: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    duration: PropTypes.number.isRequired,
+    interns: PropTypes.number.isRequired,
+    mentor: PropTypes.string.isRequired,
+    lead: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+  onClickHandler: PropTypes.func.isRequired,
+  currentExpand: PropTypes.number,
+};
+
+ListRow.defaultProps = {
+  currentExpand: null,
+};
 
 export default ListRow;
--- src/components/StreamsList/StreamsList.jsx
+++ src/components/StreamsList/StreamsList.jsx
@@ -1,182 +1,206 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  AiOutlineFilter, AiOutlineMore, AiFillCaretUp, AiFillCaretDown,
+  AiOutlineFilter,
+  AiFillCaretUp,
+  AiFillCaretDown,
 } from 'react-icons/ai';
-import OptionalMenu from '../OptionalMenu/OptionalMenu';
+import ListRow from './ListRow';
 import styles from './StreamsList.module.css';
+import currentStreams from '../../utils/currentStreams';
 
-const StreamsList = () => (
-  <section className={styles.list}>
-    <table className={styles.table}>
-      <thead>
-        <tr className={styles.header}>
-          <th>Stream Name</th>
-          <th>
-            <div className={styles.cell}>
-              Start Date
-              <div className={styles.updown}>
-                <AiFillCaretUp />
-                <AiFillCaretDown />
+const StreamsList = () => {
+  const [currentExpand, setCurrentExpand] = useState(null);
+
+  const onExpand = (index) => {
+    setCurrentExpand(index);
+  };
+
+  const renderContent = () => currentStreams.map(
+    (stream, index) => (
+      <ListRow
+        key={`${index + 1}`}
+        stream={stream}
+        index={index}
+        onClickHandler={onExpand}
+        currentExpand={currentExpand}
+      />
+    ),
+  );
+
+  return (
+    <section className={styles.list}>
+      <table className={styles.table}>
+        <thead>
+          <tr className={styles.header}>
+            <th>Stream Name</th>
+            <th>
+              <div className={styles.cell}>
+                Start Date
+                <div className={styles.updown}>
+                  <AiFillCaretUp />
+                  <AiFillCaretDown />
+                </div>
               </div>
-            </div>
-          </th>
-          <th>
-            <div className={styles.cell}>
-              Duration
-              <div className={styles.updown}>
-                <AiFillCaretUp />
-                <AiFillCaretDown />
+            </th>
+            <th>
+              <div className={styles.cell}>
+                Duration
+                <div className={styles.updown}>
+                  <AiFillCaretUp />
+                  <AiFillCaretDown />
+                </div>
               </div>
-            </div>
-          </th>
-          <th>
-            <div className={styles.cell}>
-              Interns
-              <div className={styles.updown}>
-                <AiFillCaretUp />
-                <AiFillCaretDown />
+            </th>
+            <th>
+              <div className={styles.cell}>
+                Interns
+                <div className={styles.updown}>
+                  <AiFillCaretUp />
+                  <AiFillCaretDown />
+                </div>
               </div>
-            </div>
-          </th>
-          <th>
-            <div className={styles.cell}>
-              Mentor
-              <div className={styles.filter}>
-                <AiOutlineFilter />
+            </th>
+            <th>
+              <div className={styles.cell}>
+                Mentor
+                <div className={styles.filter}>
+                  <AiOutlineFilter />
+                </div>
               </div>
-            </div>
-          </th>
-          <th>
-            <div className={styles.cell}>
-              Lead
-              <div className={styles.filter}>
-                <AiOutlineFilter />
+            </th>
+            <th>
+              <div className={styles.cell}>
+                Lead
+                <div className={styles.filter}>
+                  <AiOutlineFilter />
+                </div>
               </div>
-            </div>
-          </th>
-          <th>
-            <div className={styles.cell}>
-              Status
-              <div className={styles.filter}>
-                <AiOutlineFilter />
+            </th>
+            <th>
+              <div className={styles.cell}>
+                Status
+                <div className={styles.filter}>
+                  <AiOutlineFilter />
+                </div>
               </div>
-            </div>
-          </th>
-          <th> </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className={styles.row}>
-          <td className={styles.name}>Frontend Dev. stream 03</td>
-          <td>22 Jun 2021</td>
-          <td>31 days</td>
-          <td>20</td>
-          <td>Igor Bariev</td>
-          <td>Mark Ovchinnikov</td>
-          <td><span className={styles.oncoming}>Oncoming</span></td>
-          <td>
-            <div className={`${styles.more} ${styles.expanded}`}>
-              <div className={styles.submenu}>
-                <OptionalMenu />
+            </th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderContent()}
+          {/* <tr className={styles.row}>
+            <td className={styles.name}>Frontend Dev. stream 03</td>
+            <td>22 Jun 2021</td>
+            <td>31 days</td>
+            <td>20</td>
+            <td>Igor Bariev</td>
+            <td>Mark Ovchinnikov</td>
+            <td><span className={styles.oncoming}>Oncoming</span></td>
+            <td>
+              <div className={`${styles.more} ${styles.expanded}`}>
+                <div className={styles.submenu}>
+                  <OptionalMenu />
+                </div>
+                <AiOutlineMore />
               </div>
-              <AiOutlineMore />
-            </div>
-          </td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>C++ Dev. stream 08</td>
-          <td>01 Jun 2021</td>
-          <td>15 days</td>
-          <td>18</td>
-          <td>Anastasia Marchenko</td>
-          <td>Maxim Osipenko</td>
-          <td><span className={styles.active}>Active</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>QA stream 02</td>
-          <td>31 May 2021</td>
-          <td>31 days</td>
-          <td>24</td>
-          <td>Maxim Osipenko</td>
-          <td>Anatoly Simonov</td>
-          <td><span className={styles.paused}>Paused</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>C++ Dev. stream 08</td>
-          <td>01 Jun 2021</td>
-          <td>15 days</td>
-          <td>18</td>
-          <td>Anastasia Marchenko</td>
-          <td>Maxim Osipenko</td>
-          <td><span className={styles.active}>Active</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>C++ Dev. stream 08</td>
-          <td>01 Jun 2021</td>
-          <td>15 days</td>
-          <td>18</td>
-          <td>Anastasia Marchenko</td>
-          <td>Maxim Osipenko</td>
-          <td><span className={styles.active}>Active</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>Frontend Dev. stream 03</td>
-          <td>22 Jun 2021</td>
-          <td>31 days</td>
-          <td>20</td>
-          <td>Igor Bariev</td>
-          <td>Mark Ovchinnikov</td>
-          <td><span className={styles.oncoming}>Oncoming</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>C++ Dev. stream 08</td>
-          <td>01 Jun 2021</td>
-          <td>15 days</td>
-          <td>18</td>
-          <td>Anastasia Marchenko</td>
-          <td>Maxim Osipenko</td>
-          <td><span className={styles.active}>Active</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>QA stream 02</td>
-          <td>31 May 2021</td>
-          <td>31 days</td>
-          <td>24</td>
-          <td>Maxim Osipenko</td>
-          <td>Anatoly Simonov</td>
-          <td><span className={styles.paused}>Paused</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>QA stream 02</td>
-          <td>31 May 2021</td>
-          <td>31 days</td>
-          <td>24</td>
-          <td>Maxim Osipenko</td>
-          <td>Anatoly Simonov</td>
-          <td><span className={styles.paused}>Paused</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-        <tr className={styles.row}>
-          <td className={styles.name}>C++ Dev. stream 08</td>
-          <td>01 Jun 2021</td>
-          <td>15 days</td>
-          <td>18</td>
-          <td>Anastasia Marchenko</td>
-          <td>Maxim Osipenko</td>
-          <td><span className={styles.active}>Active</span></td>
-          <td><div className={styles.more}><AiOutlineMore /></div></td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
-);
+            </td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>C++ Dev. stream 08</td>
+            <td>01 Jun 2021</td>
+            <td>15 days</td>
+            <td>18</td>
+            <td>Anastasia Marchenko</td>
+            <td>Maxim Osipenko</td>
+            <td><span className={styles.active}>Active</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>QA stream 02</td>
+            <td>31 May 2021</td>
+            <td>31 days</td>
+            <td>24</td>
+            <td>Maxim Osipenko</td>
+            <td>Anatoly Simonov</td>
+            <td><span className={styles.paused}>Paused</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>C++ Dev. stream 08</td>
+            <td>01 Jun 2021</td>
+            <td>15 days</td>
+            <td>18</td>
+            <td>Anastasia Marchenko</td>
+            <td>Maxim Osipenko</td>
+            <td><span className={styles.active}>Active</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>C++ Dev. stream 08</td>
+            <td>01 Jun 2021</td>
+            <td>15 days</td>
+            <td>18</td>
+            <td>Anastasia Marchenko</td>
+            <td>Maxim Osipenko</td>
+            <td><span className={styles.active}>Active</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>Frontend Dev. stream 03</td>
+            <td>22 Jun 2021</td>
+            <td>31 days</td>
+            <td>20</td>
+            <td>Igor Bariev</td>
+            <td>Mark Ovchinnikov</td>
+            <td><span className={styles.oncoming}>Oncoming</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>C++ Dev. stream 08</td>
+            <td>01 Jun 2021</td>
+            <td>15 days</td>
+            <td>18</td>
+            <td>Anastasia Marchenko</td>
+            <td>Maxim Osipenko</td>
+            <td><span className={styles.active}>Active</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>QA stream 02</td>
+            <td>31 May 2021</td>
+            <td>31 days</td>
+            <td>24</td>
+            <td>Maxim Osipenko</td>
+            <td>Anatoly Simonov</td>
+            <td><span className={styles.paused}>Paused</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>QA stream 02</td>
+            <td>31 May 2021</td>
+            <td>31 days</td>
+            <td>24</td>
+            <td>Maxim Osipenko</td>
+            <td>Anatoly Simonov</td>
+            <td><span className={styles.paused}>Paused</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr>
+          <tr className={styles.row}>
+            <td className={styles.name}>C++ Dev. stream 08</td>
+            <td>01 Jun 2021</td>
+            <td>15 days</td>
+            <td>18</td>
+            <td>Anastasia Marchenko</td>
+            <td>Maxim Osipenko</td>
+            <td><span className={styles.active}>Active</span></td>
+            <td><div className={styles.more}><AiOutlineMore /></div></td>
+          </tr> */}
+        </tbody>
+      </table>
+    </section>
+  );
+};
 // StreamsList.propTypes = {
 //   children: PropTypes.node,
 // };
